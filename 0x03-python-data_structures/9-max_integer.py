a44108d1e206cd498e1a5db50ff276d4fe207b19
Abdel Hamid <abdelhamidbusiness@gmail.com>
2023-10-08 17:21:21 -0700

    stupid mistake

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list:
         maxi = my_list[0]
-        for i in max_integer:
+        for i in my_list:
             if i > maxi:
             maxi = i
         return maxi
