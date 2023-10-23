2d836c84ba329bf96012290df8a5660e0964714a
Abdel Hamid <abdelhamidbusiness@gmail.com>
2023-10-23 12:47:02 -0700

    fix few errors

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
-    except (TypeError, ValueError) as er:
-        stderr.write("Exception: {}\n".format(er))
-        return False
+        return (True)
+    except (TypeError, ValueError) as e:
+        stderr.write("Exception: {}\n".format(e))
+        return (False)
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,6 @@ from sys import stderr
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except (ZeroDivisionError, IndexError, TypeError, ValueError) as er:
-        stderr.write("Exception: {}\n".format(er))
+    except (ZeroDivisionError, IndexError, TypeError, ValueError) as e:
+        stderr.write("Exception: {}\n".format(e))
         return None
