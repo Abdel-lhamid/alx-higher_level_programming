d73514e654a56e4bde6cb632e80616a8df79d2fb
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2024-02-12 03:22:03 -0800

    first func 9-add_integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+add-integer
+Methode to return int sum of 2 ints
+"""
+
+def add_integer(a, b=98)
+    """
+    Returns sum of a and b as int
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return ( int(a) + int(b))
+
