3b7ce3094d70ec43cadcd78309d4481c55e75643
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2024-02-14 19:03:32 +0100

    fixed style

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -61,7 +61,8 @@ class Rectangle:
         """ Prints rectangle with print symbol """
         if self.__width == 0 or self.__height == 0:
             return ""
-        pic = "\n".join([str(self.print_symbol) * self.__width for rows in range(self.__height)])
+        pic = "\n".join([str(self.print_symbol) * self.__width
+            for rows in range(self.__height)])
         return pic
 
     def __repr__(self):
