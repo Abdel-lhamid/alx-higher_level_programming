89fe887ae5497e22bb4545f6269443911338cba3
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2024-02-20 01:48:47 +0100

    tasks till 8

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Module 05 base_geometry
+    class:
+        base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    an Empty Class
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Module 06-base_geometry
+
+    class:
+    BaseGeometry, with public instance method area
+"""
+
+
+class BaseGeometry:
+    """
+    Methods:
+        area(self)
+        Raises:
+        Exception area is not implemented
+    """
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
