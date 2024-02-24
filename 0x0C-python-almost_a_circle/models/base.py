966f7e387a79e8eaeaf51bcb9b3d0f49ee5b997e
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2024-02-24 13:46:03 +0100

    save_to_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
     Model 01 Base Class
     contains the base CLass
 """
+import json
 
 
 class Base():
@@ -11,8 +12,11 @@ class Base():
     Attr:
             private attribute:  __nb_objects
             public instance attribute: id
+    Funcs:
+            to_json_string(list_dictionarie)
     """
     __nb_object = 0
+
     def __init__(self, id=None):
         """
         the class constructor
@@ -26,3 +30,26 @@ class Base():
             self.id = Base.__nb_object
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file
+        """
+
+        list_dictionaries = []
+        if list_objs:
+            for obj in list_objs:
+                list_dictionaries.append(obj.to_dictionary())
+        with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_dictionaries))
