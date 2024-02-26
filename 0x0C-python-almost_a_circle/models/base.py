14a01c5b6385cf246155c3678183de7c9b159250
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2024-02-26 12:55:32 +0100

    till task 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@
     contains the base CLass
 """
 import json
+import os
 
 
 class Base():
@@ -14,6 +15,9 @@ class Base():
             public instance attribute: id
     Funcs:
             to_json_string(list_dictionarie)
+            save_to_file(cls, list_objs)
+            from_json_string(json_string)
+            create(cls, **dictionary)
     """
     __nb_object = 0
 
@@ -49,7 +53,44 @@ class Base():
 
         list_dictionaries = []
         if list_objs:
-            for obj in list_objs:
-                list_dictionaries.append(obj.to_dictionary())
+            list_dictionaries = [obj.to_dictionary() for obj in list_objs]
         with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributes already set
+        """
+        if len(dictionary) > 1:
+            rec = cls(1, 1)
+        else:
+            rec = cls(1)
+        rec.update(**dictionary)
+        return rec
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        A method that returns a list of instances
+        """
+        file_path = cls.__name__ + ".json"
+        if not os.path.exists(file_path) or os.stat(file_path).st_size == 0:
+            return []
+        with open(file_path, mode="r", encoding="utf-8") as f:
+            dictionaries = cls.from_json_string(f.read())
+        instances = []
+        if dictionaries:
+            for dic in dictionaries:
+                instances.append(cls.create(**dic))
+        return instances
+
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+    Module 10  and now the Square
+    class: Square
+    funcs:
+        __init__
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+        Class Square inherits Rectangle square
+        funcs:
+                def __init__(self, size, x=0, y=0, id=None)
+
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+            initialization
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({:d}) {:d}/{:d} - {:d}\
+                ".format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """
+            getter for size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+            setter
+        """
+        self.width = value
+        self.heigh = value
+
+    def update(self, *args, **kwargs):
+        """
+            methode that update attributes
+        """
+        funcs = ["id", "size", "x", "y"]
+        if args:
+            for i in range(len(args)):
+                setattr(self, funcs[i], args[i])
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+    def to_dictionary(self):
+        """
+        A function that returns the dictionary presentation of an obj
+        """
+        dic = {}
+        list_attrs = ["id", 'x', "size", 'y']
+        list_vals = [self.id, self.x, self.size, self.y]
+
+        for k, v in zip(list_attrs, list_vals):
+            dic[k] = v
+
+        return dic
