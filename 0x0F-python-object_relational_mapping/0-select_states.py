739b8234e9b7e86f0166b71c3c32eb8ae70aaaea
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-04-27 06:29:00 -0700

    fix style and req

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,40 +1,25 @@
 #!/usr/bin/python3
+"""
+select all states from the table states on the given db
+"""
+
+
 import MySQLdb
-import sys
+from sys import argv
 
-def select_states(username, password, database):
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
-
-        # Execute SQL query to fetch all states sorted by id
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        results = cursor.fetchall()
-
-        # Display results
-        for row in results:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL database:", e)
-        sys.exit(1)
-    finally:
-        # Close database connection
-        if db:
-            db.close()
 
 if __name__ == "__main__":
-    # Check if correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
+    # create db connection
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    # cursor
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    res = cursor.fetchall()
+    # print
+    for row in res:
+        print(row)
 
-    # Get MySQL username, password, and database name from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Call select_states function with provided arguments
-    select_states(username, password, database)
-
+    cursor.close()
+    db.close()
