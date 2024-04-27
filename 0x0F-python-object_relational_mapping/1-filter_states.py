7839e6f15378f3db3977d6902d8568f85fa1e5b5
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-04-27 09:06:41 -0700

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+from sys import argv
+from exec_query import execquery
+
+if __name__ == "__main__":
+
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    res = execquery("localhost",
+                    3306, argv[1],
+                    argv[2], argv[3], query)
+    # print
+    for row in res:
+        print(row)
