e3f05461e87377de2366d0969c549aed5d182fb8
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-04-27 13:53:19 -0700

    fix task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,9 +11,8 @@ from exec_query import execquery
 
 if __name__ == "__main__":
 
-    query = """SELECT *
-                 FROM states
-                 WHERE name LIKE '{:s}' ORDER BY id ASC""".format(argv[4])
+    query = """SELECT * FROM states
+               WHERE name LIKE BINARY '{:s}' ORDER BY id ASC""".format(argv[4])
     res = execquery("localhost",
                     3306, argv[1],
                     argv[2], argv[3], query)
