c5c2c3aa702a4ac499b4a00872a90eda841f773f
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-04-28 11:18:41 -0700

    task 11 insert

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script that adds the State object “Louisiana” to the db hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from session_maker import make_session
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    # use make_session func to get the session()
+    session = make_session(argv[1], argv[2], argv[3])
+
+    # add new state and commit to table
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
+
+    print("{:d}".format(new.id))
