0bab3aac099a09599d4dc840f60712ef348c9bd3
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-06-12 15:20:47 +0100

    task 12

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,7 @@
+$('document').ready(function () {
+  $('#btn_translate').click(function () {
+    $.get('https://fourtonfish.com/hellosalut/?lang=' + $('#language_code').val(), function (data) {
+      $('#hello').text(data.hello);
+    });
+  });
+});
